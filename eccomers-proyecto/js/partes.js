e4e6265d83d31abe59736dab757e9d0e36fd1872
lusal1207 <lu.sal1207@gmail.com>
2026-02-01 19:29:40 -0300

    Add files via upload

--- eccomers-proyecto/js/partes.js
+++ eccomers-proyecto/js/partes.js
@@ -4,3 +4,39 @@ fetch("partes/header.html")
 fetch("partes/footer.html")
 .then(res => res.text())
 .then(data => document.getElementById('footer').innerHTML = data);
+document.addEventListener("DOMContentLoaded", () => {
+                const juegos_index = [
+                {
+                    id: "GTA_V",
+                    nombre: "GTA V - 500$",
+                    imagen: "img/1.webp",
+                    },
+                {
+                    id:"Horizon_Forbidden_West",
+                    nombre: "Horizon Forbidden West- 600$",
+                    imagen: "img/2.webp",
+                },
+                {
+                    id:"TLOU",
+                    nombre: "The Last Of Us - 600$",
+                    imagen: "img/3.webp",
+                },
+                {
+                    id:"Minecraft",
+                    nombre: "Minecraft - 700$",
+                    imagen: "img/4.jpg",
+                },
+            ]
+    const contenedor = document.querySelector(".seccion-productos ul");
+    juegos_index.forEach(juego => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+        <div class="producto" id="producto">
+                        <img src="${juego.imagen}" alt="${juego.nombre}" />
+                        <h3>${juego.nombre}</h3>
+                        <button type="button" onclick="comprar('${juego.id}')">Comprar</button>
+        </div>
+        `  ;
+        contenedor.appendChild(li);
+    });
+});
